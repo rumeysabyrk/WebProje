fdc9b99c296f2b90f2c0052f3b3a83e0b9ec1013
rumeysa <rbayrak677@gmail.com>
2022-12-22 23:21:12 +0300

    veritabanı islemleri

---
+++ WebProje/DBContext/ProductDBContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebProje.DBContext
+{
+    public class ProductDBContext: DbContext
+    {
+        public ProductDBContext(DbContextOptions options): base(options) { }
+        public DbSet<ProductDBContext> Product { get; set; }
+    }
+}
--- WebProje/Startup.cs
+++ WebProje/Startup.cs
@@ -17,6 +17,7 @@ namespace WebProje
 {
     public class Startup
     {
+        
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
