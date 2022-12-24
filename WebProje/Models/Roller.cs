992df00601514260d9b7e721a2be29a6bb457455
rumeysa <rbayrak677@gmail.com>
2022-12-24 21:40:11 +0300

    rol islemlerinin bir kismi yapildi

---
+++ WebProje/Models/Roller.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebProje.Models
+{
+    public class Roller
+    {
+        public string RolAdi { get; set; }
+    }
+}
